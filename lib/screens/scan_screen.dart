3b7b86e5a25e3a7281bcd8b0e49e1699dd255734
Dizastaryo <159602209+Dizastaryo@users.noreply.github.com>
2025-01-13 00:44:30 +0500

    senu-wil::suka

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wifi_iot/wifi_iot.dart';
+import 'package:wifi_scan/wifi_scan.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class ScanScreen extends StatefulWidget {
@@ -8,11 +8,82 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
-  List<String> _wifiSSIDs = [];
+  List<WiFiAccessPoint> _wifiSSIDs = [];
+  bool _isScanning = false;
+  String _logMessage = "";
 
   @override
   void initState() {
     super.initState();
+  }
+
+  // Функция для обработки сканирования Wi-Fi
+  Future<void> _scanWifi() async {
+    setState(() {
+      _isScanning = true;
+      _logMessage = "Запрос разрешений на местоположение...";
+    });
+    _showLogMessage(_logMessage);
+
+    // Проверка разрешений на местоположение
+    var status = await Permission.location.status;
+    if (!status.isGranted) {
+      status = await Permission.location.request();
+      if (!status.isGranted) {
+        setState(() {
+          _isScanning = false;
+          _logMessage = "Разрешение на местоположение отклонено.";
+        });
+        _showLogMessage(_logMessage);
+        return;
+      }
+    }
+
+    setState(() {
+      _logMessage = "Разрешение на местоположение получено.";
+    });
+    _showLogMessage(_logMessage);
+
+    // Начало сканирования
+    try {
+      final canStart = await WiFiScan.instance.canStartScan();
+      if (canStart != CanStartScan.yes) {
+        setState(() {
+          _logMessage = "Невозможно начать сканирование.";
+        });
+        _showLogMessage(_logMessage);
+        return;
+      }
+
+      await WiFiScan.instance.startScan();
+      setState(() {
+        _logMessage = "Сканирование начато.";
+      });
+      _showLogMessage(_logMessage);
+
+      // Получение результатов сканирования
+      final results = await WiFiScan.instance.getScannedResults();
+      setState(() {
+        _wifiSSIDs = results;
+        _logMessage = "Найдено ${_wifiSSIDs.length} сетей Wi-Fi.";
+      });
+      _showLogMessage(_logMessage);
+    } catch (e) {
+      setState(() {
+        _logMessage = 'Ошибка при сканировании Wi-Fi: $e';
+      });
+      _showLogMessage(_logMessage);
+    } finally {
+      setState(() {
+        _isScanning = false;
+      });
+    }
+  }
+
+  // Функция для отображения лога
+  void _showLogMessage(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
@@ -22,39 +93,19 @@ class _ScanScreenState extends State<ScanScreen> {
         title: const Text('Wi-Fi Scanner'),
       ),
       body: Center(
-        child: ListView.builder(
-          itemCount: _wifiSSIDs.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(_wifiSSIDs[index]),
-            );
-          },
-        ),
+        child: _isScanning
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: _wifiSSIDs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_wifiSSIDs[index].ssid),
+                  );
+                },
+              ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // Check for location permission
-          var status = await Permission.location.status;
-          if (!status.isGranted) {
-            // Request location permission if not granted
-            status = await Permission.location.request();
-            if (!status.isGranted) {
-              // Permission denied, exit the function
-              return;
-            }
-          }
-
-          // Load available Wi-Fi networks
-          List<WifiNetwork?>? networks = await WiFiForIoTPlugin.loadWifiList();
-
-          setState(() {
-            // Extract only SSID from each network and display it
-            _wifiSSIDs = networks
-                    ?.map((network) => network?.ssid ?? 'Unknown')
-                    .toList() ??
-                [];
-          });
-        },
+        onPressed: _isScanning ? null : _scanWifi,
         child: Icon(Icons.search),
       ),
     );
